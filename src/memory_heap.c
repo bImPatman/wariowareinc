3624ed47ff277bc5cc95031a23a6b98353ea9083
bImPatman <94498604+bImPatman@users.noreply.github.com>
2026-06-25 11:58:47 -0600

    Add memory heap allocation function

--- src/memory_heap.c
+++ src/memory_heap.c
@@ -4,6 +4,12 @@ asm(".include \"include/gba.inc\"");
 
 #include "asm/memory_heap/asm_08006174.s"
 
+s32 func_08006174(s32, s32);
+
+s32 mem_heap_alloc(s32 x) {
+  func_08006174(0, x);
+}
+
 #include "asm/memory_heap/asm_08006184.s"
 
 #include "asm/memory_heap/asm_08006240.s"
